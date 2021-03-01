037a0e3216c4fe25c31e284e8719b80f1db87040
Nishat Sayyed <nishat.sayyed@morganrandall.com>
2021-03-01 16:23:46 +0530

    Initial commit

---
+++ app/Http/Controllers/LegoItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LegoItem;
+use Illuminate\Http\Request;
+
+class LegoItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LegoItem  $legoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LegoItem $legoItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LegoItem  $legoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LegoItem $legoItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LegoItem  $legoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LegoItem $legoItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LegoItem  $legoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LegoItem $legoItem)
+    {
+        //
+    }
+}
---
+++ app/Models/LegoItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LegoItem extends Model
+{
+    use HasFactory;
+}
