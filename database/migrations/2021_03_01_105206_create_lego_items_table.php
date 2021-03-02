4a0af7ec246f347075973b188cd6cf91c32c6168
Nishat Sayyed <nishat.sayyed@morganrandall.com>
2021-03-02 16:37:49 +0530

    added migration to create lego_item table

--- database/migrations/2021_03_01_105206_create_lego_items_table.php
+++ database/migrations/2021_03_01_105206_create_lego_items_table.php
@@ -15,6 +15,16 @@ class CreateLegoItemsTable extends Migration
     {
         Schema::create('lego_items', function (Blueprint $table) {
             $table->id();
+            $table->enum('marketplace', ['US', 'UK']);
+            $table->string('name');
+            $table->integer('number');
+            $table->string('url');
+            $table->double('price');
+            $table->double('sale_price');
+            $table->double('discount_amount');
+            $table->double('discount_percentage');
+            $table->timestamp('date_spotted');
+            $table->enum('stock_status', ['Available', 'Out of stock']);
             $table->timestamps();
         });
     }
