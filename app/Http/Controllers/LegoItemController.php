a7f75334d0284c07bb15fa863878036c5c5d6024
Nishat Sayyed <nishat.sayyed@morganrandall.com>
2021-03-02 16:38:29 +0530

    added temporary data processing in controller

--- app/Http/Controllers/LegoItemController.php
+++ app/Http/Controllers/LegoItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\LegoItem;
 use Illuminate\Http\Request;
+use Goutte\Client;
 
 class LegoItemController extends Controller
 {
@@ -14,7 +15,49 @@ class LegoItemController extends Controller
      */
     public function index()
     {
-        //
+        $market = request()->query('market');
+        $priceDelimiter = $market == 'us' ? '$' : '£';
+        $url = $market == 'us' ? 'https://www.lego.com/en-us/categories/retiring-soon' : 'https://www.lego.com/en-gb/categories/retiring-soon';
+        $client = new Client();
+        $crawler = $client->request('GET', $url);
+        $result = $crawler->filter('li[data-test="product-item"] div[data-test="product-leaf"]')->each(function ($node) use ($market, $priceDelimiter) {
+            $marketplace = $market == 'us' ? 'US' : 'UK';
+            $url = $node->filter('[data-test="product-leaf-title-link"]')->first()->attr('href');
+            $name = $node->filter('[data-test="product-leaf-title"]')->text();
+            $itemNumber = substr($url, strrpos($url, '-') + 1);
+            $priceText = $node->filter('[data-test="product-price"]')->text();
+            $price = floatval(str_replace($priceDelimiter, '', substr($priceText, strrpos($priceText, $priceDelimiter))));
+            $salePrice = null;
+            if ($node->filter('[data-test="product-price-sale"]')->count() > 0) {
+                $salePriceText = $node->filter('[data-test="product-price-sale"]')->text();
+                $salePrice = floatval(str_replace($priceDelimiter, '', substr($salePriceText, strrpos($salePriceText, $priceDelimiter))));
+            }
+            $discountAmount = 0;
+            if ($salePrice != null && $price > $salePrice) {
+                $discountAmount = floatval(number_format((float) $price - $salePrice, 2, '.', ''));
+            }
+            $discountPercentage = 0;
+            if ($discountAmount != 0) {
+                $discountPercentage = round(($discountAmount * 100) / $price);
+            }
+            $status = 'Available';
+            if ($node->filter('span[type="outOfStock"]')->count() > 0) {
+                $status = 'Out of stock';
+            }
+            return [
+                'marketplace' => $marketplace,
+                'name' => $name,
+                'url' => 'www.lego.com' . $url,
+                'item_number' => $itemNumber,
+                'price' => $price,
+                'sale_price' => $salePrice,
+                'discount_amount' => $discountAmount,
+                'discount_percentage' => $discountPercentage,
+                'date_spotted' => null,
+                'stock_status' => $status
+            ];
+        });
+        return $result;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LegoItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LegoItemController::class, 'index']);
