1bf9e9244e4e6d04d1cf4a201490ecd42fee9679
Nishat Sayyed <nishat.sayyed@morganrandall.com>
2021-03-03 18:18:44 +0530

    added logic for merging scraped results and db records

--- app/Http/Controllers/LegoItemController.php
+++ app/Http/Controllers/LegoItemController.php
@@ -5,26 +5,105 @@ namespace App\Http\Controllers;
 use App\Models\LegoItem;
 use Illuminate\Http\Request;
 use Goutte\Client;
+use function _\difference;
 
 class LegoItemController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $market = request()->query('market');
-        $priceDelimiter = $market == 'us' ? '$' : '£';
         $url = $market == 'us' ? 'https://www.lego.com/en-us/categories/retiring-soon' : 'https://www.lego.com/en-gb/categories/retiring-soon';
         $client = new Client();
+
         $crawler = $client->request('GET', $url);
+
+        // Crawl the first page
+        $result = $this->getResultsFromCrawler($crawler, $market);
+        // Get total number of pages
+        $pages = $this->getTotalPages($crawler);
+        // Crawl other pages starting from page 2
+        for ($i = 2; $i <= $pages; $i++) {
+            $newUrl = "$url?page=$i";
+            $crawler = $client->request('GET', $newUrl);
+            $newResult = $this->getResultsFromCrawler($crawler, $market);
+            $result = array_merge($result, $newResult);
+        }
+
+        // Reset database
+        // LegoItem::truncate();
+        // LegoItem::insert($result);
+
+        $this->mergeChangesWithDb($result, $market);
+
+        $legos = LegoItem::where(['marketplace' => $market == 'us' ? 'US' : 'UK'])->get();
+
+        return ['total' => count($result), 'totalFromDb' => count($legos), 'result' => $result, 'fromDb' => $legos];
+    }
+
+    private function mergeChangesWithDb($scrapedResults, $market)
+    {
+        // NOTE: $scrapedResults always takes precedence over db records.
+        $legos = LegoItem::where(['marketplace' => $market == 'us' ? 'US' : 'UK'])->get();
+        // to sync db with scraped result, find out data that is present in db but not in scraped result
+        foreach ($legos as $index => $lego) {
+            $legoSerialized = $lego->makeHidden(['id', 'created_at', 'updated_at'])
+                ->toArray();
+            $filteredArray = array_filter($scrapedResults, function ($result) use ($legoSerialized) {
+                return $legoSerialized['number'] == $result['number'];
+            });
+            if (count($filteredArray) == 0) {
+                // if record not found in scrapedResults - delete record from db, email update (use delete email template)
+                $lego->delete();
+                // update in-memory db collection
+                unset($legos[$index]);
+            }
+        }
+
+        // iterate through each scraped result
+        foreach ($scrapedResults as $index => $result) {
+            // for each scraped result try to get record from db where item number matches the number in scraped result
+            $filteredArray = array_filter($legos->toArray(), function ($lego) use ($result) {
+                return $lego['number'] == $result['number'];
+            });
+
+            if (count($filteredArray) > 0) {
+                // if element found - check if there are any changes between scraped result and the db record
+                // leaving date_spotted, id, created_at, updated_at
+                foreach ($filteredArray as $lego) {
+                    $diff = array_diff($lego, $result);
+                    if (count($diff) > 0) {
+                        // if changes found - update db record with scraped result and email update (use update email template)
+                        LegoItem::where(['number' => $result['number']])->update($result);
+                    }
+                }
+            } else {
+                //  if element not found - create a new record in db with this scraped result and email update (use create email template)
+                LegoItem::insert($result);
+            }
+        }
+    }
+
+    private function getTotalPages($crawler)
+    {
+        $pagesSummary = $crawler->filter('div[data-test="listing-summary"]')->text();
+        $itemsPerPage = 17;
+        $totalItems = intval(explode(' ', $pagesSummary)[5]);
+        $pageNumbers = (int)($totalItems / $itemsPerPage);
+        if ($totalItems % $itemsPerPage > 0) {
+            $pageNumbers = $pageNumbers + 1;
+        }
+        return $pageNumbers;
+    }
+
+    private function getResultsFromCrawler($crawler, $market)
+    {
+        $priceDelimiter = $market == 'us' ? '$' : '£';
         $result = $crawler->filter('li[data-test="product-item"] div[data-test="product-leaf"]')->each(function ($node) use ($market, $priceDelimiter) {
             $marketplace = $market == 'us' ? 'US' : 'UK';
             $url = $node->filter('[data-test="product-leaf-title-link"]')->first()->attr('href');
             $name = $node->filter('[data-test="product-leaf-title"]')->text();
-            $itemNumber = substr($url, strrpos($url, '-') + 1);
+            $itemNumber = intval(substr($url, strrpos($url, '-') + 1));
             $priceText = $node->filter('[data-test="product-price"]')->text();
             $price = floatval(str_replace($priceDelimiter, '', substr($priceText, strrpos($priceText, $priceDelimiter))));
             $salePrice = null;
@@ -47,82 +126,16 @@ class LegoItemController extends Controller
             return [
                 'marketplace' => $marketplace,
                 'name' => $name,
+                'number' => $itemNumber,
                 'url' => 'www.lego.com' . $url,
-                'item_number' => $itemNumber,
                 'price' => $price,
                 'sale_price' => $salePrice,
                 'discount_amount' => $discountAmount,
                 'discount_percentage' => $discountPercentage,
-                'date_spotted' => null,
+                'date_spotted' => now()->toDateString(),
                 'stock_status' => $status
             ];
         });
         return $result;
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\LegoItem  $legoItem
-     * @return \Illuminate\Http\Response
-     */
-    public function show(LegoItem $legoItem)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\LegoItem  $legoItem
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(LegoItem $legoItem)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\LegoItem  $legoItem
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, LegoItem $legoItem)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\LegoItem  $legoItem
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(LegoItem $legoItem)
-    {
-        //
     }
 }
