547c36efce1fc403b3fab21d1f069c0f9a9d619e
Nishat Sayyed <nishat.sayyed@morganrandall.com>
2021-03-03 18:18:10 +0530

    changed date_spotted column to date type

---
+++ database/migrations/2021_03_03_105323_change_date_spotted.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeDateSpotted extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lego_items', function (Blueprint $table) {
+            $table->date('date_spotted')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lego_items', function (Blueprint $table) {
+            $table->timestamp('date_spotted')->change();
+        });
+    }
+}
