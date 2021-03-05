4619d07ed36656f886f221799d9b7b953ff31e7b
Nishat Sayyed <nishat.sayyed@morganrandall.com>
2021-03-05 18:47:34 +0530

    added vuejs and tailwind to the frontend

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Example Component</div>
+
+          <div v-for="lego in legos" :key="lego.id" class="card-body">
+            {{ lego.name }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      legos: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      fetch("/api/legos")
+        .then((response) => response.json())
+        .then((response) => (this.legos = response));
+    },
+  },
+};
+</script>
